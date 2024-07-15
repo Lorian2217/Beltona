8b307c4cab52649bef8cf1bbd9c06a06984da930
Loran <danilmockhnatkin@gmail.com>
2024-07-15 15:53:06 +0400

    Данила Мохнаткин
    Первые шаги (3)

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-07-15 11:33:50 UTC
+#Software: Joomla! 5.1.0 Stable [ Kudumisha ] 16-April-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-07-15T11:33:50+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-07-15T11:35:05+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-07-15T11:35:45+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
