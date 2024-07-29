b34436cf2d9050e975844a81ec97c0a5ed573983
Loran <danilmockhnatkin@gmail.com>
2024-07-29 18:03:33 +0400

    Начало вёрстки. Работа с шапкой

--- templates/lorian-4/index.php
+++ templates/lorian-4/index.php
@@ -10,16 +10,30 @@
 <body class="<?php echo $active->alias; ?>">
 <header id="navbar" class="navbar navbar-expand-xl navbar-light">
 	<div class="container">
+		<a href="/" class="main__logo">
+			<img src="/images/main_logo.png" alt="main_logo">
+		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div id="navbar-content" class="collapse navbar-collapse">
+		<div id="navbar-content" class="collapse navbar-collapse justify-content-between">
 			<div id="top_menu" class="navbar-nav">
 				<jdoc:include type="modules" name="top_menu" />
 			</div>
 			<div id="header_items" class="navbar-nav">
 				<jdoc:include type="modules" name="header_item" />
 			</div>
+			<div class="d-flex flex-column justify-content-center me-4">
+				<div class="d-flex align-items-center">
+					<div class="image"> <img src="/images/social/phone.png" alt="phone"> </div>
+					<a href="tel:+79055955161">+79055955161</a>
+				</div>
+				<div class="d-flex align-items-center">
+					<div class="image"> <img src="/images/social/mail.png" alt="phone"> </div>
+					<a href="mailto:mail@beltona.ru">mail@beltona.ru</a>
+				</div>
+			</div>
+			<button type="button" class="btn btn-default">Заказать</button>
 		</div>
 	</div>
 </header>
